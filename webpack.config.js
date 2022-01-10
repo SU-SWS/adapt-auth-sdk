975775b9bdcca147e5df3fc380ed2f0efc1c5b74
Nathan Plowman <nathan.plowman@ffwagency.com>
2022-01-10 11:39:26 -0700

    ADAPT-4167: Further build debugging.

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './build.ts',
+  entry: './index.ts',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
